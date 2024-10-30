4ea5fc9d6ab1534b51435dbb1f6abdf4e57b066a
Rishabh <rishabh.mh1@gmail.com>
2024-10-30 18:09:50 +0530

    package lock deleted

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/thar-diary",
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: "/thar-dairy/",
-};
+const nextConfig = {};
 
 export default nextConfig;
