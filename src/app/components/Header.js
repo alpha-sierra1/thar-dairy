b0038bf77260d7e04d26932802f465525433ca0a
Rishabh <rishabh.mh1@gmail.com>
2024-10-31 01:33:31 +0530

    padding fix

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -16,10 +16,7 @@ export default function Header() {
 
   return (
     <header className='bg-white'>
-      <nav
-        aria-label='Global'
-        className='mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8'
-      >
+      <nav aria-label='Global' className='mx-auto flex max-w-7xl items-center justify-between p-4'>
         <div className='flex lg:flex-1'>
           <a href='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>Thar Dairy</span>
